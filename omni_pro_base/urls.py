63ad332538b7b694b7d7c773d72e1f592da27a61
Julián Valdés <julianvaldes24@gmail.com>
2024-03-20 16:27:07 -0500

    refactor(omni_pro_base): remove user and group router setup from urls
    Remove the DefaultRouter setup and associated URL patterns for UserViewSet and GroupViewSet. The endpoints for users, groups, and default DRF auth have been cleaned up, simplifying the URL configuration in omni_pro_base/urls.py.

--- omni_pro_base/urls.py
+++ omni_pro_base/urls.py
@@ -1,13 +1,5 @@
 from django.urls import include, path
-from omni_pro_base.views import GroupViewSet, UserViewSet
-from rest_framework import routers
-
-router = routers.DefaultRouter()
-router.register(r"users", UserViewSet)
-router.register(r"groups", GroupViewSet)
 
 urlpatterns = [
     path(r"base/", include('health_check.urls')),
-    path("", include(router.urls)),
-    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]
